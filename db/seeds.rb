d3c5530c0119b683d4fe28bca2a142f0caed8eb0
Oli Finn <oliver.james.finn@gmail.com>
2022-02-22 14:11:01 +0000

    fake data added from faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seeding..."
+10.times do
+  name = Faker::Hipster.word
+  price = rand(100..1000)
+  height = rand(120..220)
+  years_of_service = rand(0..30)
+  Agent.create(name: name, price: price, height: height, years_of_service: years_of_service)
+  puts name
+end
