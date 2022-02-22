10f6e12a956ed3d01b6b67a1e4b902550b59f25c
Seb Naylor <sebnaylor@gmail.com>
2022-02-22 16:16:15 +0000

    Created bookings index skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :agents, only: [:show, :index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :create, :new]
+  resources :users, only: [:index, :create, :new] do
+    resources :bookings, only: [:show, :update]
+  end
+  resources :agents, only: [:new, :create, :index, :show] do
+    resources :bookings, only: [:new]
+  end
+  resources :bookings, only: [:create, :index]
 end
