bb9fb7295d8f36053d43361c085cc77c2191551f
jack glen <jackglen0@gmail.com>
2022-02-22 12:42:21 +0000

    agent controller methods added

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,2 +1,18 @@
 class AgentsController < ApplicationController
+  def new
+    @agent = Agent.new
+  end
+
+  def create
+    @agent = Agent.new(list_params)
+    @agent.save
+    render :new
+  end
+
+  private
+
+  def list_params
+    params.require(:agent).permit(:name, :price, :skills, :category, :height, :years_of_service, :weapon, :evilness, :ineptitude, :gadgets)
+  end
+
 end
